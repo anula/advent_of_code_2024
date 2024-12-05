2dc25472c01ccf0a0a8dbc04765082d687812862
anula <anula@users.noreply.github.com>
2024-12-05 15:27:39 +0100

    Day 5 star 2
    This one had me definitely confused for a bit ;)

--- day5/src/solution_star1.rs
+++ day5/src/solution_star1.rs
@@ -67,61 +67,16 @@ impl Solution {
             panic!("Order not odd!");
         }
 
+        dprintln!("Good order: {:?}", order);
         let mid_idx = order.len() / 2;
 
         Some(order[mid_idx])
     }
 
-    fn dfs(&self, all: &HashSet<i64>, v: i64, visited: &mut HashSet<i64>, rev_topo: &mut Vec<i64>) {
-        if !visited.insert(v) { return }
-        if let Some(neighs) = self.graph.get(&v) {
-            for n in neighs {
-                if !all.contains(&n) { continue };
-                self.dfs(all, *n, visited, rev_topo);
-            }
-        }
-        rev_topo.push(v);
-    }
-
-    fn is_topo_why_not(&self, order: &[i64]) -> bool {
-        let mut before = HashSet::new();
-        for p in order {
-            if let Some(neighs) = self.graph.get(p) {
-                for n in neighs {
-                    if before.contains(n) {
-                        println!("{:?} in order, its n: {:?} is before", p, n);
-                        return false;
-                    }
-                }
-            }
-            before.insert(p);
-        }
-        true
-    }
-
-    fn sum_incorrect_middles(&self) -> i64 {
-        let mut sum = 0;
-        for i in 0..self.pages.len() {
-            if self.is_correct_then_mid(&self.pages[i]).is_some() { continue; }
-            let mut rev_topo = Vec::new();
-            let mut visited = HashSet::new();
-            let all = HashSet::from_iter(self.pages[i].iter().cloned());
-            for j in 0..self.pages[i].len() {
-                self.dfs(&all, self.pages[i][j], &mut visited, &mut rev_topo);
-            }
-            if rev_topo.len() % 2 != 1 { panic!("rev_topo not odd!"); }
-            if rev_topo.len() != self.pages[i].len() { panic!("rev_topo incorrect!"); }
-            let topo: Vec<i64> = rev_topo.iter().cloned().rev().collect::<Vec<i64>>();
-            if !self.is_correct_then_mid(&topo).is_some() {
-                println!("order: {:?}", self.pages[i]);
-                println!("rev_topo: {:?}", rev_topo);
-                let _ = self.is_topo_why_not(&topo);
-                panic!("rev_topo not topo!")
-            }
-            let idx = rev_topo.len() / 2;
-            sum += rev_topo[idx];
-        }
-        sum
+    fn sum_correct_middles(&self) -> i64 {
+        self.pages.iter()
+            .filter_map(|p| self.is_correct_then_mid(&p))
+            .sum()
     }
 }
 
@@ -129,7 +84,7 @@ fn solve<R: BufRead, W: Write>(input: R, mut output: W) {
     let lines_it = BufReader::new(input).lines().map(|l| l.unwrap());
     let solution = Solution::from_input(lines_it);
 
-    writeln!(output, "{}", solution.sum_incorrect_middles()).unwrap();
+    writeln!(output, "{}", solution.sum_correct_middles()).unwrap();
 }
 
 pub fn main() {
@@ -182,7 +137,7 @@ mod tests {
             75,97,47,61,53
             61,13,29
             97,13,75,29,47",
-            "123",
+            "143",
         );
     }
 }
