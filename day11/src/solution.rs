02774f50223f9f37f2d8af4745f08ca712f2f701
anula <anula@users.noreply.github.com>
2024-12-10 21:48:53 -0800

    Day 11 star 2

--- day11/src/solution.rs
+++ day11/src/solution.rs
@@ -3,7 +3,7 @@ use std::io::{BufRead, BufReader, Write};
 //use regex::Regex;
 //use lazy_static::lazy_static;
 //use std::collections::HashSet;
-//use std::collections::HashMap;
+use std::collections::HashMap;
 
 macro_rules! dprintln {
     ( $( $x:expr ),* ) => {
@@ -41,47 +41,70 @@ fn split_number(num: i64) -> (i64, i64) {
     (num / ten_power(len/2), num % ten_power(len/2))
 }
 
+fn blink(stones: &[i64]) -> Vec<i64> {
+    let mut new_stones = Vec::new();
+    for s in stones {
+        match s {
+            0 => new_stones.push(1),
+            e if digits(*e) % 2 == 0 =>  {
+                let (a, b) = split_number(*e);
+                new_stones.push(a);
+                new_stones.push(b);
+            }
+            o => new_stones.push(o * 2024),
+        }
+    }
+    new_stones
+}
+
+fn many_blinks(stones: &[i64], num: i64, cache: &mut HashMap<(i64, i64), i64>) -> i64 {
+    if num == 0 {
+        return stones.len() as i64;
+    }
+    if stones.len() == 1 {
+        if let Some(res) = cache.get(&(stones[0], num)) {
+            return *res;
+        }
+        let new_stones = blink(stones);
+        let res = many_blinks(&new_stones, num - 1, cache);
+        cache.insert((stones[0], num), res);
+        return res;
+    }
+    let mut res = 0;
+    for i in 0..stones.len() {
+        res += many_blinks(&stones[i..(i+1)], num, cache);
+    }
+    res
+}
+
 impl Solution {
+    fn from_slice(sl: &[i64]) -> Self {
+        Solution {
+            stones: sl.to_vec(),
+        }
+    }
+
     fn from_input<I>(mut lines: I) -> Self
         where I: Iterator<Item = String>
     {
         let l = lines.next().unwrap();
         let line = l.trim();
 
-        Solution {
-            stones: line.split_whitespace().map(|x| x.parse::<i64>().unwrap()).collect(),
-        }
+        let stones: Vec<i64> = line.split_whitespace().map(|x| x.parse::<i64>().unwrap()).collect();
+        Solution::from_slice(&stones)
     }
 
-    fn blink(&mut self) {
-        let mut new_stones = Vec::new();
-        for s in &self.stones {
-            match s {
-                0 => new_stones.push(1),
-                e if digits(*e) % 2 == 0 =>  {
-                    let (a, b) = split_number(*e);
-                    new_stones.push(a);
-                    new_stones.push(b);
-                }
-                o => new_stones.push(o * 2024),
-            }
-        }
-        self.stones = new_stones;
-    }
-
-    fn solve(&mut self) -> i64 {
-        for _ in 0..25 {
-            self.blink();
-        }
-        return self.stones.len() as i64
+    fn solve(&self, num: i64) -> i64 {
+        let mut cache = HashMap::new();
+        return many_blinks(&self.stones, num, &mut cache);
     }
 }
 
 fn solve<R: BufRead, W: Write>(input: R, mut output: W) {
     let lines_it = BufReader::new(input).lines().map(|l| l.unwrap());
-    let mut solution = Solution::from_input(lines_it);
+    let solution = Solution::from_input(lines_it);
 
-    writeln!(output, "{}", solution.solve()).unwrap();
+    writeln!(output, "{}", solution.solve(75)).unwrap();
 }
 
 pub fn main() {
@@ -105,9 +128,15 @@ mod tests {
 
     #[test]
     fn sample() {
-        test_ignore_whitespaces(
-            "125 17",
-            "55312",
-        );
+        let input = vec![125, 17];
+        let sol = Solution::from_slice(&input);
+        assert_eq!(sol.solve(25), 55312);
+    }
+
+    #[test]
+    fn bigger() {
+        let input = vec![20, 82084, 1650, 3, 346355, 363, 7975858, 0];
+        let sol = Solution::from_slice(&input);
+        assert_eq!(sol.solve(25), 172484);
     }
 }
